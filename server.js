3d40ed52ca840e560f016571056fa267fa97d007
Igor Yanchynskyi <59307393+Icorcka@users.noreply.github.com>
2024-04-02 21:54:25 +0300

    Adding sqlite db

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./database');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+async function initializeDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected successfully.');
+        startServer();
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+function startServer() {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+initializeDatabase();
